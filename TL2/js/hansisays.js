6f012774c8683694aa1126ed695ef0668ea4a474
micharulez <38750281+micharulez@users.noreply.github.com>
2018-06-04 22:18:45 +0200

    Update hansisays.js

--- TL2/js/hansisays.js
+++ TL2/js/hansisays.js
@@ -4,7 +4,7 @@ var score;
 var highscore = 0;
 
 var currentArray = [];   //das aktuelle Array von nextColor.php
-var counter = 0;    //zählt Anzahl der eingaben pro Runde ab
+var counter = 0;    //zählt Anzahl der eingaben pro Runde ab für checkArray()
 
 $(document).ready(function () {
 
@@ -59,7 +59,7 @@ function startGameloop(seq) {      //mit dieser Funktion werden die Farben JSON 
     var sequence = seq;
 
     sequence = JSON.stringify(sequence);
-    addNewColor(sequence);
+    addNewColor(sequence);         //schicken des JSON codierten Arrays zum AJAX Get Befehl
 
     disableButtons();
 
@@ -69,7 +69,7 @@ function startGameloop(seq) {      //mit dieser Funktion werden die Farben JSON 
 
 }
 
-function updateScore(){
+function updateScore(){            //erhört den Score und passt gegebenenfalls den Highscore an
   if(score > highscore){
     highscore = score;
   }
@@ -77,19 +77,19 @@ function updateScore(){
   document.getElementById('highscore').innerHTML = highscore;
 }
 
-function checkArray(sequence){
-
-  switch (sequence[0]) {
+function checkArray(sequence){        //überprüft nach Aufruf mit Array aus Farbfolgen, ob die gedrückten Tasten
+                                        //der Reihenfolge der Farben im Array übereinstimmt
+  switch (sequence[0]) {                // unterteilung in Fälle bzw. in jeweilige Farben
     case "red":
-      if (currentArray[counter] == "red"){
-              counter++;
-              if (counter == currentArray.length) {
-                counter = 0;
+      if (currentArray[counter] == "red"){    //wenn die geklickte Farbe mit der Farbe des Arrays übereinstimmt
+              counter++;                      //wird die Laufvariable erhöht
+              if (counter == currentArray.length) {     //wurde die Farbabfolge erfolgreich eingegeben
+                counter = 0;                            //wird die Laufvariable zurückgesetzt und die Buttons deaktiviert
                 disableButtons();
                 document.getElementById('proceedButton').disabled = false;
               }
-            } else {
-              disableButtons()
+            } else {              //bei falscher Eingabe werden die Buttons deaktiviert und ein Fehrlerton gespielt
+              disableButtons()      // sowie die Laufvariable zurückgesetzt
               counter = 0;
               setTimeout(function() {
                  $('.colorbutton').addClass('pressed');
@@ -164,25 +164,22 @@ function checkArray(sequence){
 }
 
 
-function playSequence(sequence) {
+function playSequence(sequence) {         //diese Funktion spielt Farbsequenzen und einzelne Farben ab
     var toneLength = 500;
-    for (i=0; i<sequence.length; i++) {
-       switch (sequence[i]) {
-           // $('#'+squence[i])
+    for (i=0; i<sequence.length; i++) {       //ein Array mit Farbsequenzen wird durchlaufen
+       switch (sequence[i]) {         //verschiedene Farbfälle
             case "red":
                 setTimeout(function () {
-                    $(red).addClass('pressed');
-                    // timeout for button back off
+                    $(red).addClass('pressed');     //es wird zunächst eine Farbe als gedrückt markiert
                     setTimeout(function () {
-                        $(red).removeClass('pressed');},450);
-                    redSound.play()},
+                        $(red).removeClass('pressed');},450);     //dann wird die Farbe wieder normal angezeigt
+                    redSound.play()},       //während die Farbe als gedrückt angezeigt wird, wird auch der entsprechende Ton abgespielt
                      i * toneLength);
 
                 break;
             case "green":
                 setTimeout(function () {
                     $(green).addClass('pressed');
-                    // timeout for button back off
                     setTimeout(function () {
                         $(green).removeClass('pressed');},450);
                     greenSound.play()},
@@ -191,7 +188,6 @@ function playSequence(sequence) {
             case "yellow":
                 setTimeout(function () {
                     $(yellow).addClass('pressed');
-                    // timeout for button back off
                     setTimeout(function () {
                         $(yellow).removeClass('pressed');},450);
                     yellowSound.play()},
@@ -200,7 +196,6 @@ function playSequence(sequence) {
             case "blue":
                 setTimeout(function () {
                     $(blue).addClass('pressed');
-                    // timeout for button back off
                     setTimeout(function () {
                         $(blue).removeClass('pressed');},450);
                     blueSound.play()},
@@ -211,31 +206,31 @@ function playSequence(sequence) {
 
 }
 
-function handleColorClick(event, sequence, position) {
+function handleColorClick(event, sequence, position) {    //verschickt Buttoncklicks zu den jeweiligen Funktionen
     var seq = [event.target.id];
     playSequence(seq);
     checkArray(seq);
 }
 
-function addNewColor(sequence) {
+function addNewColor(sequence) {     //Hier wird ein Array zum php script geschickt und ein Array als antwort empfangen
     var seq;
     $.ajax({
       type: 'GET',
       url: 'php/nextColor.php',
-      data: {sequence: sequence},
+      data: {sequence: sequence},    //Das Array was in die php Datei gehört muss "sequence" heißen
       dataType: 'JSON',
-      success: function(data, status){
+      success: function(data, status){    //Bei erfolgreichem senden wird ein Array empfangen
          seq = data;
-         playSequence(seq.sequence);
-         currentArray = seq.sequence;
+         playSequence(seq.sequence);  //schicken zum abspielen des neuen Arrays
+         currentArray = seq.sequence; //speichern des Arrays in einer Globalen Variable
       },
-      error: function(jqXHR, textStatus){
+      error: function(jqXHR, textStatus){   //Wenn das verschicken fehlerhaft war wird dies angezeigt
         alert('Request failed: ' + textStatus);
       }
     });
 }
 
-function disableButtons() {
+function disableButtons() {       //alle Buttons werden deaktiviert
     document.getElementById('newGameButton').disabled = true;
     document.getElementById('proceedButton').disabled = true;
     var colorButtons = document.getElementsByClassName('colorbutton');
@@ -244,14 +239,14 @@ function disableButtons() {
     }
 }
 
-function enableButtons() {
+function enableButtons() {    //Farbbuttons werden aktiviert
     var colorButtons = document.getElementsByClassName('colorbutton');
     for (i=0; i<colorButtons.length; i++) {
         colorButtons[i].addEventListener("click", handleColorClick);
     }
 }
 
-function copyArray(input) {
+function copyArray(input) {   //wurde nicht benutzt
     var copy = input.slice();
     return copy;
 }
